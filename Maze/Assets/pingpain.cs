e503a15bcc3f82fa01aba17b35851b1bb911fcab
TylerBarger33 <47096087+TylerBarger33@users.noreply.github.com>
2019-03-10 21:29:31 -0400

    challenge2
    challenge 2

---
+++ Maze/Assets/pingpain.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pingpain : MonoBehaviour
+{
+    public Transform startMarker;
+    public Transform endMarker;
+    public float speed = 1.0F;
+    private float startTime;
+    private float journeyLength;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Time.time;
+        journeyLength = Vector2.Distance(startMarker.position, endMarker.position);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float distCovered = (Time.time - startTime) * speed;
+        float fracJourney = distCovered / journeyLength;
+        transform.position = new Vector2(Mathf.PingPong(Time.time, 11), 2);
+
+
+
+    }
+}
---
+++ Maze/Assets/scripts/PlayerController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public Text loseText;
+    public Text winText;
+    public Text countText;
+    public Text livesText;
+    private Rigidbody2D rb2d;
+    private int count;
+    private int lives;
+    public float speed;
+    public float jumpForce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        loseText.text = "";
+        lives = 3;
+        winText.text = "";
+        countText.text = "Count: " + count.ToString();
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey("escape"))
+            Application.Quit();
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, 0);
+        rb2d.AddForce(movement * speed);
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            countText.text = "Count: " + count.ToString();
+            if (count >= 4)
+            {
+                winText.text = "You Win!";
+            }
+        }
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.SetActive(false);
+            lives = lives - 1;
+            livesText.text = "Lives: " + lives.ToString();
+            if (lives <= 0)
+            {
+                loseText.text = "Game Over";
+                UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex);
+            }
+        }
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+
+        if (collision.collider.tag == "Ground")
+        {
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                rb2d.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+                
+            }
+        }
+    }
+}
+        
+        
+    
+
+
+
