56d555f26ffd5adacbc138d2f83fd3fede9ca067
TylerBarger33 <47096087+TylerBarger33@users.noreply.github.com>
2019-04-07 21:11:54 -0400

    challenge 3
    space shooter extended

---
+++ Challenge 3/Assets/Scripts/GameController.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject[] hazards;
+    public Vector3 spawnValues;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+
+    public Text Restart;
+    public Text GameOver;
+    public Text ScoreText;
+    public Text Win;
+
+    private bool gameOver;
+    private bool restart;
+    private int score;
+
+    void Start()
+    {
+        gameOver = false;
+        restart = false;
+        Restart.text = "";
+        GameOver.text = "";
+        Win.text = "";
+
+        score = 0;
+        UpdateScore();
+        StartCoroutine (SpawnWaves());
+    }
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+            Application.Quit();
+
+        if (restart)
+        {
+            if (Input.GetKeyDown(KeyCode.T))
+            {
+                SceneManager.LoadScene("Challenge3");
+            }
+        }
+    }
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                GameObject hazard = hazards[Random.RandomRange(0, hazards.Length)];
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(startWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+            if (gameOver)
+            {
+                Restart.text = "Press 'T' to Reset";
+                restart = true;
+                break;
+            }
+        }
+    }
+
+    public void AddScore(int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore();
+    }
+
+    void UpdateScore()
+    {
+        ScoreText.text = "Points: " + score;
+        if (score >= 100)
+        {
+            Win.text = "You win!" + System.Environment.NewLine + "Game Created By Tyler Barger";
+            gameOver = true;
+            restart = true;
+        }
+    }
+    public void gameover()
+    {
+        GameOver.text = "Game Over!";
+        gameOver = true;
+    }
+
+}
---
+++ Challenge 3/Assets/Scripts/WeaponController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+    public float delay;
+    private AudioSource audiosource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audiosource = GetComponent<AudioSource>();
+        InvokeRepeating("Fire", delay, fireRate);
+    }
+    void Fire ()
+    {
+        Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+        audiosource.Play();
+    }
+}  
